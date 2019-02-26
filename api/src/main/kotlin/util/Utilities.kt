f1a79dba01ce805eb4cf2362567952bc9081d42b
ITesserakt <potryas66@mail.ru>
2019-02-26 22:21:22 +0300

    Обновлена динамическая активность

--- api/src/main/kotlin/util/Utilities.kt
+++ api/src/main/kotlin/util/Utilities.kt
@@ -1,6 +1,9 @@
 package util
 
 import discord4j.core.`object`.util.Snowflake
+import reactor.core.publisher.Mono
+import reactor.core.publisher.toMono
+import reactor.util.function.Tuple2
 import java.awt.Color
 import java.math.BigInteger
 import java.util.*
@@ -11,7 +14,7 @@ fun <T> T?.toOptional(): Optional<T> = Optional.ofNullable(this)
 operator fun String.times(num: Int): String =
         this.repeat(num)
 
-infix fun Number.`**`(num : Number):Double =
+infix fun Number.`**`(num: Number): Double =
         Math.pow(this.toDouble(), num.toDouble())
 
 fun Throwable.prettyPrint() = StringBuilder(this.localizedMessage ?: "")
@@ -26,3 +29,6 @@ fun BigInteger.toSnowflake(): Snowflake = Snowflake.of(this)
 
 val RandomColor
     get() = Color(Random.nextInt(256), Random.nextInt(256), Random.nextInt(256))
+
+fun <T1 : Any, T2 : Any> Mono<T1>.zipWith(other: T2): Mono<Tuple2<T1, T2>> =
+        this.zipWith(other.toMono())
--- impl/src/main/kotlin/main.kt
+++ impl/src/main/kotlin/main.kt
@@ -1,9 +1,11 @@
-
 import command.GuildCommandProvider
+import discord4j.core.DiscordClient
 import discord4j.core.DiscordClientBuilder
 import discord4j.core.`object`.entity.Member
 import discord4j.core.`object`.entity.MessageChannel
 import discord4j.core.`object`.entity.Role
+import discord4j.core.`object`.presence.Activity
+import discord4j.core.`object`.presence.Presence
 import discord4j.core.event.domain.guild.MemberJoinEvent
 import discord4j.core.event.domain.lifecycle.ReadyEvent
 import discord4j.core.event.domain.message.MessageCreateEvent
@@ -14,13 +16,19 @@ import modules.AdminModule
 import modules.DeveloperModule
 import modules.HelpModule
 import modules.RainbowModule
+import reactor.core.Disposable
+import reactor.core.publisher.toMono
 import reactor.util.Logger
 import reactor.util.Loggers
+import reactor.util.function.component1
+import reactor.util.function.component2
 import types.MemberResolver
 import types.MessageChannelResolver
 import types.ResolverProvider
 import types.RoleResolver
 import util.Database
+import util.zipWith
+import java.time.Duration
 import java.time.LocalTime
 import java.util.*
 
@@ -35,6 +43,15 @@ fun main() {
     val props = ResourceBundle.getBundle("config")
     val client = DiscordClientBuilder(props.getString("token")).build()
 
+    specifyEvents(client)
+    specifyModules()
+    setupResolvers()
+    enableDynamicPresence(client)
+
+    client.login().block()
+}
+
+private fun specifyEvents(client: DiscordClient) {
     with(client.eventDispatcher) {
         on(ReadyEvent::class.java)
                 .subscribe { ReadyEventHandler().handle(it) }
@@ -43,12 +60,22 @@ fun main() {
         on(MemberJoinEvent::class.java)
                 .subscribe { JoinHandler().handle(it) }
     }
-
-    specifyModules()
-    setupResolvers()
-
-    client.login().block()
 }
+
+fun enableDynamicPresence(client: DiscordClient): Disposable = client.users.count()
+        .zipWith(0)
+        .mergeWith(client.guilds.count().zipWith(1))
+        .mergeWith(client.regions.count().zipWith(2))
+        .mergeWith(client.guilds.flatMap { it.channels }.count().zipWith(3))
+        .mergeWith(Thread.activeCount().toLong().toMono().zipWith(4))
+        .delayElements(Duration.ofSeconds(5))
+        .map { (value, index) ->
+            val words = arrayOf("users", "guilds", "regions", "channels", "threads")
+            "$value ${words[index]}"
+        }.doOnNext {
+            client.updatePresence(Presence.online(Activity.watching("$it | say !help"))).subscribe()
+        }.repeat()
+        .subscribe()
 
 fun setupResolvers() {
     logger.info("Starting loading of resolvers...")
