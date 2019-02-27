d4c48ac7758b387739290d9a98a01436d59b449c
ITesserakt <potryas66@mail.ru>
2019-02-27 19:55:25 +0300

    Удалил дублированные файлы

--- src/main/kotlin/ru/tesserakt/bot/rainbow/util/Config.kt
+++
@@ -1,8 +0,0 @@
-package ru.tesserakt.bot.rainbow.util
-
-private val db = Database
-private fun configQuarryFor(key : String) = "SELECT value FROM config WHERE key='$key'"
-
-val token = db.execute<String>(configQuarryFor("token"))
-const val version = "0.0.7-prerelease"
-val debug = db.execute<String>(configQuarryFor("debug_mode")).toBoolean()
