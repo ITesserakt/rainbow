31ba039fba4559c069a13b2cf1a8b9ec21d8e3e7
ITesserakt <potryas66@mail.ru>
2019-02-27 19:55:25 +0300

    Удалил дублированные файлы

--- src/main/kotlin/ru/tesserakt/bot/rainbow/core/types/StringResolver.kt
+++
@@ -1,7 +0,0 @@
-package ru.tesserakt.bot.rainbow.core.types
-
-import ru.tesserakt.bot.rainbow.core.context.ICommandContext
-
-class StringResolver : ITypeResolver<String> {
-    override fun read(context: ICommandContext, input: String): String = input
-}
